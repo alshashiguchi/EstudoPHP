502f12463808a6fd4c38830a969d5c21f91adebb
Andre Luiz Hashiguchi <alshashiguchi@gmail.com>
2016-04-13 23:00:43 -0300

    MassAssingment

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 //somente pelo fato de trabalhar com o extends model já funciona com o model do laravel
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content'        
+    ];
 }
