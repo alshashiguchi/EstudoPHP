c950463776e8c77863e09dbf4accc5524afd3e3f
Andre Luiz Hashiguchi <alshashiguchi@gmail.com>
2016-04-14 01:30:40 -0300

    Listando Utiliznado Relacionamentos

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,13 @@
     
     @foreach ($posts as $post)
         <h2>{{ $post->title }} <i>({{ $post->created_at }})</i></h2>
-        <p>{{ $post->content }}</p>
+        <p>{{ $post->content }}</p>        
+        <b>Tags:</b><br>
+        <ul>
+            @foreach ($post->tags as $tag)
+                <li>{{ $tag->name }}</li>
+            @endforeach
+        </ul>
         <hr>
         
         <h3>Comments</h3>
