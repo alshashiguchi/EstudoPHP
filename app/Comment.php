cd5530c61179a684dc3268cc5788ccf84a31e69f
Andre Luiz Hashiguchi <alshashiguchi@gmail.com>
2016-04-14 00:25:18 -0300

    Fazendo Relacionamentos

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable=[
+        'post_id',
+        'comment',
+        'name',
+        'email'
+    ];
+    
+    public function post()
+    {
+        //Retorna qual o post referente ao comment (belongsTo)
+        return $this->belongsTo('App\Post');
+    }
 }
